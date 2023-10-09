5aa08d67e593467862be6a451901fe3e0c9d2f54
chenyou <chenyouhuang@gmail.com>
2023-10-09 15:21:58 -0400

    Implement privacy pages

--- components/EntryRelatedPages/LegalComponents/LegalContentContainer.tsx
+++ components/EntryRelatedPages/LegalComponents/LegalContentContainer.tsx
@@ -2,15 +2,69 @@ import React from "react";
 
 import Paper from "@mui/material/Paper";
 import Box from "@mui/material/Box";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 import SkoolRulesContent from "./SkoolRulesContent";
+import PrivacyPolicyContent from "./PrivacyPolicyContent";
+
+const theme = createTheme({
+  typography: {
+    h1: {
+      fontFamily: "Roboto, Helvetica, Arial, sans-serif",
+      fontWeight: "bold",
+      fontStyle: "normal",
+      lineHeight: "1.5",
+      fontSize: "28px",
+      margin: "16px 0 16px 0",
+    },
+    h2: {
+      fontFamily: "Roboto, Helvetica, Arial, sans-serif",
+      fontWeight: "bold",
+      fontStyle: "normal",
+      lineHeight: "1.5",
+      fontSize: "20px",
+      margin: "16px 0 16px 0",
+    },
+    h3: {
+      display: "block",
+      fontSize: "1.17em",
+      fontWeight: "bold",
+      margin: "16px 0 16px 0",
+    },
+    body1: {
+      margin: "16px 0 16px 0",
+    },
+  },
+  components: {
+    MuiList: {
+      styleOverrides: {
+        root: {
+          listStyleType: "disc",
+          padding: "0 0 0 40px",
+        },
+      },
+    },
+    MuiListItem: {
+      styleOverrides: {
+        root: {
+          display: "list-item",
+          padding: "0",
+          lineHeight: "1.5",
+        },
+      },
+    },
+  },
+});
 
 export default function LegalContentContainer() {
   return (
-    <Box sx={{ backgroundColor: "white" }}>
-      <Paper sx={{ padding: "32px" }}>
-        <SkoolRulesContent />
-      </Paper>
-    </Box>
+    <ThemeProvider theme={theme}>
+      <Box sx={{ backgroundColor: "white" }}>
+        <Paper sx={{ padding: "32px" }}>
+          {/* <SkoolRulesContent /> */}
+          <PrivacyPolicyContent />
+        </Paper>
+      </Box>
+    </ThemeProvider>
   );
 }
