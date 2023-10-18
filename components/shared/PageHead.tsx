7b8b6e18c5a698a6771d376fff1b00928bea7fb6
chenyou <chenyouhuang@gmail.com>
2023-10-17 21:07:06 -0400

    Check pricing page

--- components/shared/PageHead.tsx
+++ components/shared/PageHead.tsx
@@ -4,21 +4,28 @@ import Head from "next/head";
 interface PageHeadProps {
   title: string;
   content: string;
+  favicon?: string;
 }
 
 /**
  * PageHead component renders the head elements for related pages.
  * @param {string} title - The title of the page.
  * @param {string} content - The meta description content of the page.
+ * @param {string} favicon - The location of the favicon (optional).
  */
-
-export default function PageHead({ title, content }: PageHeadProps) {
+const PageHead: React.FC<PageHeadProps> = ({
+  title,
+  content,
+  favicon = "/skool-icon.ico",
+}) => {
   return (
     <Head>
       <title>{title}</title>
       <meta name="description" content={content} />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link rel="icon" href="/skool-icon.ico" />
+      <link rel="icon" href={favicon} />
     </Head>
   );
-}
+};
+
+export default PageHead;
--- pages/pricing.tsx
+++ pages/pricing.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-
 import {
   EntryRelatedPageContainer,
   EntryRelatedPageTitle,
@@ -9,9 +8,11 @@ import {
 import PageHead from "@/components/shared/PageHead";
 
 export default function Pricing() {
+  // Page metadata
   const headTitle = "Skool: Pricing";
   const headDescriptionContent =
     "Simple pricing. 1 plan with everything included. No hidden fees. Get started with a 14-day free trial. Cancel anytime.";
+
   return (
     <>
       <PageHead title={headTitle} content={headDescriptionContent} />
